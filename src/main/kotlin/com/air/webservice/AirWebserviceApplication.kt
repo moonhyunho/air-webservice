7525e15b1c1168818c953d774cfcc28056c1dbf6
moonhyunho <moonh_2@naver.com>
2020-05-22 15:30:38 +0900

    Initial commit

---
+++ src/main/kotlin/com/air/webservice/AirWebserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.air.webservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AirWebserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<AirWebserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/air/webservice/AirWebserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.air.webservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AirWebserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
